1fe75018abd4101314ec350dbfcf6e3b1d69b9bd
jeonghyeonPark <shshjhjh4455@gmail.com>
2022-09-30 17:41:05 +0900

    tensor사용
    text_to_word_sequence로 토큰화 진행
    
    -->>textrank 사용해서 수정필요!

--- imdb_reviews_crawling.py
+++ imdb_reviews_crawling.py
@@ -1,19 +1,30 @@
+from collections import Counter
 import requests
+import nltk
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import pandas as pd
-import re
-import jpype
 from konlpy.tag import Okt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from nltk.tokenize import word_tokenize
+from nltk import sent_tokenize
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
 
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
 
-def rating_transfer(rating):
-    if rating > 5:
-        rating = 1
-    elif rating <= 5:
-        rating = 0
-    pn = rating
-    return pn
+# def rating_transfer(rating):
+#     if rating > 5:
+#         rating = 1
+#     elif rating <= 5:
+#         rating = 0
+#     pn = rating
+#     return pn
 
 
 review_list = []
@@ -62,9 +73,9 @@ while flag:
         if len(rating) == 2:
             rating = rating[0].text
             review = item.select(".text")[0].text
-            pn = rating_transfer(int(rating))
-            review_list.append(review)
+            pn = int(rating)
             rating_list.append(pn)
+            review_list.append(review)
             cnt = cnt + 1
         else:
             rating = ""
@@ -80,54 +91,88 @@ while flag:
     else:
         flag = False
 
-df = pd.DataFrame(columns=["review", "label"])
+df = pd.DataFrame(columns=["label","review" ])
 df["review"] = review_list
 df["label"] = rating_list
 df.to_csv("IMDB_reviews.csv")
 
 df.info()
-# 코멘트가 없는 리뷰 데이터(NaN) 제거
-df_reviews = df.dropna()
-# 중복 리뷰 제거
-df_reviews = df_reviews.drop_duplicates(["review"])
 
-# "title" 행 생성
-# df_reviews["title"] = "IMDB" # 영화 제목 입력
+## 문장 토큰화
+#review 컬럼을 반복하여 sent_tokenize()를 이용해 문장 단위로 분리
+sentences = []
+for s in df['review']:
+    sentences.extend(sent_tokenize(s))
+print("분리된 문장 개수:", len(sentences))
 
-# 긍정, 부정 리뷰 수
-print(df_reviews.groupby("label").size().reset_index(name="count"))
+'''
+#문장 단위로 분리된 데이터를 확인
+for i in range(len(sentences)):
+    print('{}번 째 문장: {}\n'.format(i+1, sentences[i]))
+'''
 
-# 긍정, 부정 리뷰 활용하기 위한 변수 선언
-pos_reviews = df_reviews[df_reviews["label"] == 1]
-neg_reviews = df_reviews[df_reviews["label"] == 0]
+##단어 토큰화
+#문장 단위로 분리된 데이터를 반복하여 text_to_word_sequence() 를 이용해 단어 단위로 분리
+words = []
+for sentence in sentences:
+    temp_X = text_to_word_sequence(sentence)
+    words.extend(temp_X)
+
+'''
+#단어 단위로 분리된 데이터를 확인
+for i in range(len(words)):
+    print('{}번 째 단어: {}\n'.format(i+1, words[i]))
+'''
+
+##불용어 제거
+#불용어 목록을 불러온 후, 단어 단위로 분리된 데이터를 반복하여 불용어 제거
+stop_words = nltk.corpus.stopwords.words('english')
+words = [word for word in words if word not in stop_words]
+
+# words의 단어 길이가 1이하인 단어 제거
+words = [word for word in words if len(word) > 1]
+
+# words의 단어가 숫자인 단어 제거
+words = [word for word in words if not word.isnumeric()]
+
+# words의 단어가 영어가 아닌 단어 제거
+words = [word for word in words if word.isalpha()]
+
+# words의 단어를 소문자로 변환
+words = [word.lower() for word in words]
+
+# words의 단어의 품사가 명사, 형용사, 부사, 동사인 단어만 추출
+tagged = nltk.pos_tag(words)
+words = [word for word, tag in tagged if tag in ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']]
+
+##불용어 제거 후 단어 개수 확인
+print("불용어 제거 후 단어 개수:", len(words))
+
+'''
+##불용어 제거 후 단어 확인
+for i in range(len(words)):
+    print('{}번 째 단어: {}\n'.format(i+1, words[i]))
+'''
+
+##표제어 추출
+#단어 단위로 분리된 데이터를 반복하여 표제어 추출
+lemmatizer = nltk.stem.WordNetLemmatizer()
+words = [lemmatizer.lemmatize(w) for w in words]
+
+##표제어 추출 후 단어 개수 확인
+print("표제어 추출 후 단어 개수:", len(words))
 
 
-# 리뷰 영어 이외의 문자 제거
-def clean_text(text):
-    text = re.sub("[^a-zA-Z]", " ", text)
-    return text
+##표제어 추출 후 단어 확인
+for i in range(len(words)):
+    print('{}번 째 단어: {}\n'.format(i+1, words[i]))
+
+##단어 빈도수 확인
+#단어 단위로 분리된 데이터를 반복하여 단어 빈도수 확인
+word_count = Counter(words)
+print("단어 빈도수:", word_count)
 
 
 
-# 긍정 리뷰 영어 이외의 문자 제거
-pos_reviews["review"] = pos_reviews["review"].map(lambda x: clean_text(x))
-# 부정 리뷰 영어 이외의 문자 제거
-neg_reviews["review"] = neg_reviews["review"].map(lambda x: clean_text(x))
-
-
-# 형태소 분석
-def tokenize(doc):
-    okt = Okt()
-    return [" ".join(okt.morphs(s)) for s in doc]
-
-
-# 긍정 리뷰 형태소 분석
-pos_reviews["review"] = tokenize(pos_reviews["review"])
-# 부정 리뷰 형태소 분석
-neg_reviews["review"] = tokenize(neg_reviews["review"])
-
-# 긍정 리뷰 형태소 분석 결과 확인
-print(pos_reviews["review"].head())
-# 부정 리뷰 형태소 분석 결과 확인
-print(neg_reviews["review"].head())
+#######textrank 사용해서 수정!
 
