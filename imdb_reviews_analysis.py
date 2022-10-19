a9f3411828d55d1f27ff3dc929fca9a50632f638
jeonghyeonPark <shshjhjh4455@gmail.com>
2022-10-19 17:45:33 +0900

    imdb review crawling

--- imdb_reviews_analysis.py
+++ imdb_reviews_analysis.py
@@ -34,15 +34,25 @@ movie_name = [
     "Busanhaeng",
     "Gaetmaeul Chachacha",
 ]
-for i in tqdm(range(len(movie_name)), mininterval=1, desc="progress_analysis"):
-    df = pd.read_csv(movie_name[i] + "_review.csv")
+for k in tqdm(range(len(movie_name)), mininterval=1, desc="progress_analysis"):
+    df = pd.read_csv(movie_name[k] + "_review.csv")
 
     # 모든 리뷰를 하나의 문자열로 합침
     review_text = df["review"].str.cat(sep=" ")
-    
+
     # review_text를 text_to_word_sequence()를 이용해 단어 단위로 분리
     tokens = text_to_word_sequence(review_text)
     print("단어 개수:", len(tokens))
+
+    # 불용어 제거
+    stop_words = stopwords.words("english")
+    tokens = [w for w in tokens if not w in stop_words]
+
+    # 단어가 숫자인 경우 제거
+    tokens = [word for word in tokens if word.isalpha()]
+
+    # tokens에 중복된 단어 제거
+    tokens = list(set(tokens))
 
     # 사전 훈련된 GloVe
     glove_dict = dict()
@@ -61,30 +71,44 @@ for i in tqdm(range(len(movie_name)), mininterval=1, desc="progress_analysis"):
     embedding_dim = 100
     zero_vector = np.zeros(embedding_dim)
 
-    # 단어 벡터의 평균으로부터 문장 벡터를 얻는다.
-    def calculate_sentence_vector(sentence):
+    # tokens 리스트를 이용해 문장의 벡터 표현을 만든다.
+    sentence_vectors = []
+    for sentence in tqdm(tokens, desc="sentence"):
         if len(sentence) != 0:
-            return sum([glove_dict.get(word, zero_vector) for word in sentence]) / len(
+            v = sum([glove_dict.get(w, zero_vector) for w in sentence]) / len(
                 sentence
             )
         else:
-            return zero_vector
+            v = zero_vector
+        sentence_vectors.append(v)
+    
+    # sentence_vectors를 이용해 각 단어의 중요도를 계산한다.
+    sim_mat = np.zeros([len(tokens), len(tokens)])
+    for i in tqdm(range(len(tokens)), desc="sim_mat"):
+        for j in range(len(tokens)):
+            if i != j:
+                sim_mat[i][j] = cosine_similarity(
+                    sentence_vectors[i].reshape(1, embedding_dim),
+                    sentence_vectors[j].reshape(1, embedding_dim),
+                )[0, 0]
 
-    # 문장에 대해서 calculate_sentence_vector()를 적용
-    sentences = sent_tokenize(review_text)
-    sentence_vectors = [calculate_sentence_vector(text_to_word_sequence(sentence)) for sentence in sentences]
-
-    # 문장 벡터 간의 코사인 유사도 행렬을 구한다.
-    similarity_matrix = cosine_similarity(sentence_vectors)
-
-    # 코사인 유사도 행렬을 그래프로 표현
-    nx_graph = nx.from_numpy_array(similarity_matrix)
+    # sim_mat를 이용해 네트워크를 생성한다.
+    nx_graph = nx.from_numpy_array(sim_mat)
     scores = nx.pagerank(nx_graph)
 
-    # 페이저랭크 알고리즘으로 구한 점수를 기준으로 문장을 정렬
-    ranked_sentences = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+    # 중요도가 높은 순으로 정렬한다.
+    ranked_words = sorted(
+        ((scores[i], s) for i, s in tqdm(enumerate(tokens),desc="ranked_words")), reverse=True
+    )
 
-    # 상위 3개의 문장을 출력
-    for i in range(3):
-        print(ranked_sentences[i][1])
-        print()
+    # 중요도가 높은 순으로 단어를 출력한다.
+    for i in range(10):
+        print(ranked_words[i][1])
+
+    # csv 파일로 저장
+    df = pd.DataFrame(ranked_words)
+    df.to_csv(movie_name[k] + "_word_summary.csv", index=False)
+    print(movie_name[k] + " word summary complete")
+
+
+    
